2821b6f330b684976f451f077634b335f4c748ef
Mansur Sulejmanov <mansur.sulejmanov@student.ap.be>
2024-06-26 14:27:27 +0200

    Add project files.

---
+++ TheGameGame/Game1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TheGameGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        
+        private Texture2D texture;
+        private Rectangle deelRectangle;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            deelRectangle = new Rectangle(0, 0, 300, 300);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = Content.Load<Texture2D>("SpriteSheet");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Green);
+
+            _spriteBatch.Begin();
+
+            // Draw the entire texture
+            _spriteBatch.Draw(texture, new Vector2(10, 10), deelRectangle, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TheGameGame/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new TheGameGame.Game1();
+game.Run();
